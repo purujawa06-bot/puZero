0c2dcf73ede2af2b9f57471aa84b0d95b92ae303
Ricky <purujawa06@gmail.com>
2026-03-15 13:07:16 +0000

    Back

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -7,6 +7,5 @@ router.get('/category', indexController.getCategory);
 router.get('/features', indexController.getFeatures);
 router.get('/tos', indexController.getTOS);
 router.get('/privacy', indexController.getPrivacy);
-router.get('/history', indexController.getHistory);
 
 module.exports = router;
